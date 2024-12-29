bce323232d72921c012dd78e019f86943b6609e9
shivansh nayak <shivansh@vnnovate.com>
2024-12-29 17:40:36 +0530

    changes in home screen

--- src/components/homeScreen.js
+++ src/components/homeScreen.js
@@ -6,6 +6,7 @@ function HomeScreen(props) {
     return (
         <div>
             <h1>hello world this is home screen</h1>
+            <h2>So in this project we will go through the crud operation using react</h2>
         </div>
     );
 }
